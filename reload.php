4c5fb8f338ae90343af9cebf6ed0f8c14025ec82
Ricardo Lafuente <r@manufacturaindependente.org>
2016-05-08 13:33:27 +0200

    Melhorar script de reload

--- reload.php
+++ reload.php
@@ -1,15 +1,23 @@
-<?php
-function execPrint($command) {
-  $result = array();
-  exec($command, $result);
-  foreach ($result as $line) {
-    print($line . "\n");
-  }
-}
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <?php
+    function execPrint($command) {
+      $result = array();
+      exec($command, $result);
+      foreach ($result as $line) {
+        print($line . "\n");
+      }
+    }
+    echo '<p>Tá atualizado! <a href="index.php">Ver o resultado.</a>';
 
-echo '<p>Tá feito! <a href="index.php">Ver o resultado.</a>';
-
-// Get the latest version of the repository
-print("<pre>" . execPrint("git pull") . "</pre>");
-
-?>
+    // Get the latest version of the repository
+    print("<pre>");
+    execPrint("git pull");
+    print("</pre>");
+    ?>
+    <a href="pages.php">Vamos começar, então</a>
+  </body>
+</html>
