c1c727ca504caa6595a058a354338a96bbba07ed
Ricardo Lafuente <r@manufacturaindependente.org>
2016-05-08 02:59:25 +0200

    Já funciona \o/

--- pages.php
+++ pages.php
@@ -1,19 +1,28 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
 <?php
 require_once 'Michelf/Markdown.inc.php';
 
 if ( $_GET["titulo"] ) {
-  $file_to_load = $_GET["titulo"];
+  $file_to_load = $_GET["titulo"] . ".md";
 } else {
   // pick a random file from the texts dir
   $page_files_list = scandir('textos');
-  $file_to_load = array_rand($page_files_list);
+  unset($page_files_list[0]);
+  unset($page_files_list[1]);
+  $file_to_load = $page_files_list[array_rand($page_files_list)];
 }
 
 // get the file contents and render them into HTML
-$page_contents_markdown = file_get_contents($file_to_load);
-$page_contents_html = Markdown::defaultTransform($page_contents_markdown);
+$page_contents_markdown = file_get_contents('textos/' . $file_to_load);
+$md = new \Michelf\Markdown();
+$page_contents_html = $md->defaultTransform($page_contents_markdown);
+
 // show the HTML
 echo $page_contents_html;
 ?>
 
-<a href="pages.php">Vai</a>
+<a href="pages.php">Vai</a></body>
